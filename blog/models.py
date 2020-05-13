cc003dc20d3667fa1cb3bc020dea1fbc31cdcd15
Vitor Marinho <vitormarinho0610@gmail.com>
2020-05-13 17:05:18 -0300

    alteracoes well 5

--- blog/models.py
+++ blog/models.py
@@ -5,12 +5,12 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from ckeditor.fields import RichTextField
-from taggit.managers import TaggableManager
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(blank=True, null=True)
-    parent = models.ForeignKey('self',blank=True, null=True ,related_name='children' ,on_delete=models.CASCADE)
+    
 
     class Meta:
         #enforcing that there can not be two categories under a parent with same slug
@@ -18,23 +18,17 @@ class Category(models.Model):
         # __str__ method elaborated later in post.  use __unicode__ in place of
         
         # __str__ if you are using python 2
-
-        unique_together = ('slug', 'parent',)    
+  
         verbose_name_plural = "categories"  
 
     def __str__(self):                           
-        full_path = [self.name]                  
-        k = self.parent
-        while k is not None:
-            full_path.append(k.name)
-            k = k.parent
-        return ' -> '.join(full_path[::-1])
+        return self.name                  
+        
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.CASCADE)
-    tags = TaggableManager()
+    categories = models.ManyToManyField('Category', null=True, blank=True)
     photo = models.ImageField(upload_to='images/', null = True, blank = True)
     text = RichTextField(blank=True,null=True)
     created_date = models.DateTimeField(default=timezone.now)
--- blog_well/settings.py
+++ blog_well/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'ckeditor',
-    'taggit',
 ]
 
 MIDDLEWARE = [
